e72026a783fbc738d80738b3ee8d337f9e4f5271
Nathan Bean <nhbean@WIN2.CS.KSU.EDU>
2022-03-03 09:13:31 -0600

    Initial commit

---
+++ TilemapExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TilemapExercise
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new ExampleGame())
+                game.Run();
+        }
+    }
+}
